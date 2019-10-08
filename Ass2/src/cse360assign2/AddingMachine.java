b6a66c12b817cc5bd58db231120cbafc92a5c437
mitchwesting <mitchwesting@mitchs-air.3520.dhcp.asu.edu>
2019-10-08 16:18:05 -0700

    Second Commit

--- Ass2/src/cse360assign2/AddingMachine.java
+++ Ass2/src/cse360assign2/AddingMachine.java
@@ -15,25 +15,54 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
+	private String history;
 	
+	/**
+	* Constructor, initializes the total variable to 0
+	* 
+	*/
 	public AddingMachine () {
-		total = 0;  // not needed - included for clarity
+	total = 0; // not needed - included for clarity
+	history="0";
 	}
 	
+	/**
+	* Returns the current total
+	* 
+	* @return total 
+	*/
 	public int getTotal () {
-		return 0;
+	return total;
 	}
 	
+	/**
+	* Adds the value to the current total, and stores the operation
+	* 
+	* @param value an int value that will be added to the current total
+	*/
 	public void add (int value) {
-		
+	total = total + value;
+	history += "+";
+	history += Integer.toString(value);
 	}
 	
+	/**
+	* Subtracts the vale to the current total, and stores the operation
+	* 
+	* @param value an int value that will be subrracted from the current total
+	*/
 	public void subtract (int value) {
-		
+	total = total - value;
+	history += " - ";
+	history += Integer.toString(value);
 	}
-		
+	
+	/**
+	* Prints the history of operations used
+	* 
+	*/
 	public String toString () {
-		return "";
+	return history;
 	}
 
 	public void clear() {
