09ee93a6b5f85da5b343cfdb966b708bbe0ce96b
mitchwesting <mitchwesting@mitchs-air.3520.dhcp.asu.edu>
2019-10-08 16:06:20 -0700

    First commit

---
+++ Ass2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,42 @@
+
+/*
+ * Mitch Westing
+ * CSE360
+ * Assignment2
+ */
+/**
+ * This class Adds and Subtracts from the current total,
+ * and keeps a log of all operations already done,
+ * which can be printed by the toString method
+ * 
+ */
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+		
+	public String toString () {
+		return "";
+	}
+
+	public void clear() {
+	
+	}
+}
